639a8a64b839a5c8338c874d6470c42536e70bf9
Azaly MAHAVITENY <azmaha@sio.sisr>
2026-03-25 09:53:46 +0100

    Page connexion fini avec du style.

---
+++ FrontOffice/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Connexion</title>
+</head>
+<body>
+<div class="card">
+  <div class="logo-wrap">
+        <img src="../Images/logo-caisse.png" alt="CaisseShop" class="logo-img">
+    </div>
+
+<h1>Connexion</h1>
+
+<div class="form-group">
+<label for="Email">Mail : </label>
+<input type="email" name="Mail" id="Email">
+</div>
+
+<div class="form-group">
+<label for="MotDePasse">Mot de passe : </label>
+<input type="password" name="MDP" id="MotDePass">
+</div>
+
+<button type="submit" class="btn-connect">Se connecter</button>
+
+</div>
+</body>
+</html>
